6d0f10e1eabf63a1f35f4406c3623ac94eb00e17
R.Murzakov <romuald.murzakov@gmail.com>
2017-09-19 10:53:07 +0100

    Cleaned up files of Pop-up window

--- client/src/views/welcome_pop_up_window.js
+++ client/src/views/welcome_pop_up_window.js
@@ -7,7 +7,7 @@ WelcomePopUpWindow.prototype.render = function(){
   this.text = "This is my text in Pop-up Box";
 
   var popUp = document.getElementById('pop-up');
-  // popUp.id = "pop-up";
+
 
   var closeButton = document.createElement('button');
   closeButton.innerText = "X";
@@ -28,30 +28,3 @@ WelcomePopUpWindow.prototype.render = function(){
 }
 
 module.exports = WelcomePopUpWindow;
-
-
-// // Get the modal
-// var modal = document.getElementById('myModal');
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal 
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
